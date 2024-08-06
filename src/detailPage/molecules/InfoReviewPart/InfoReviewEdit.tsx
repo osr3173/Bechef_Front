159fde1b9a355ef7579c94eb72e8f9785bf8e7dc
앙귀머랭 <156579536+osr3173@users.noreply.github.com>
2024-08-06 23:24:04 +0900

    Add files via upload

---
+++ src/detailPage/molecules/InfoReviewPart/InfoReviewEdit.tsx
@@ -0,0 +1,75 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import InfoReviewStarComponent from "./InfoReviewStarComponent";
+
+type InfoReviewEditProps = {
+  review_id: number;
+  currentComment: string;
+  currentRating: number;
+  onEditSubmit: (
+    review_id: number,
+    reviewContent: string,
+    reveiw_rating: number
+  ) => void;
+  onCancel: () => void;
+};
+//상세 페이지 - 리뷰 수정 컴포넌트
+const InfoReviewEdit = ({
+  review_id,
+  currentComment,
+  currentRating,
+  onEditSubmit,
+  onCancel,
+}: InfoReviewEditProps) => {
+  const [editReview, setEditReview] = useState(currentComment); //현재 댓글 상황
+  const [editRating, setEditRating] = useState(currentRating); //현재 별점 상황
+  //0801 글자수 제한 및 글자 수 표현 추가
+  const [inputCount, setInputCount] = useState(currentComment.length); //글자수 count
+  const maxCommentLength = 200;
+
+  const handleUpdateComment = () => {
+    if (editReview.trim()) {
+      onEditSubmit(review_id, editReview, editRating);
+    }
+  };
+
+  //0801 추가
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setEditReview(e.target.value);
+    setInputCount(e.target.value.length);
+  };
+
+  // currentComment가 변경되었을 때 inputCount를 업데이트하는 useEffect 추가
+  useEffect(() => {
+    setInputCount(currentComment.length);
+  }, [currentComment]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <InfoReviewStarComponent
+        totalStars={5}
+        setReviewRating={setEditRating} // 별점 수정 시 상태 업데이트
+        initialRating={editRating} // 초기 별점 설정
+        reset={false}
+      />
+      <textarea
+        value={editReview}
+        onChange={handleInputChange} //0801 추가
+        className="border p-2 resize-none"
+        maxLength={maxCommentLength}
+      />
+      <div className="text-right text-sm">
+        {inputCount}/{maxCommentLength}
+      </div>
+      <div className="flex gap-2">
+        <button className="w-px31" onClick={handleUpdateComment}>
+          저장
+        </button>
+        <button className="w-px31" onClick={onCancel}>
+          취소
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default InfoReviewEdit;
---
+++ src/detailPage/molecules/InfoReviewPart/InfoReviewList.tsx
@@ -0,0 +1,139 @@
+import { useCallback, useEffect, useState } from "react";
+import InfoReviewComponent, {
+  InfoReviewComponentProps,
+} from "./InfoReviewComponent";
+import axios from "axios";
+import InfoReviewEditBox from "./InfoReviewEditBox";
+import { jwtDecode } from "jwt-decode";
+// import { log } from "console";
+import {
+  INFO_REVEIW_UPDATE, //오타
+  INFO_REVIEW_DELETE,
+  INFO_REVIEW_LIST,
+} from "../../../Urls/URLList";
+
+type InfoReviewListProps = {
+  store_id: number;
+  infoNewReviewList: InfoReviewComponentProps[];
+  fetchAverageRating: () => void;
+};
+
+const InfoReviewList = ({
+  store_id,
+  infoNewReviewList,
+  fetchAverageRating,
+}: InfoReviewListProps) => {
+  const [infoReviewList, setInfoReviewList] = useState<
+    InfoReviewComponentProps[]
+  >([]);
+  const [currentUser, setCurrentUser] = useState<any>(null);
+
+  const getToken = () => {
+    return localStorage.getItem("jwt-token");
+  };
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      try {
+        const decodedToken: any = jwtDecode(token);
+        // console.log("Decoded token:", decodedToken);
+        setCurrentUser(decodedToken);
+      } catch (error) {
+        console.error("토큰 디코딩 중 오류 발생:", error);
+      }
+    }
+  }, []);
+
+  const fetchReview = useCallback(async () => {
+    try {
+      const token = getToken();
+      const response = await axios.get(INFO_REVIEW_LIST(store_id), {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data: InfoReviewComponentProps[] = response.data.reverse();
+      // console.log("리뷰 데이터: ", data);
+      setInfoReviewList(data);
+    } catch (error) {
+      console.error("리뷰 정보를 가져오는 중 오류 발생: ", error);
+    }
+  }, [store_id]);
+
+  useEffect(() => {
+    fetchReview();
+  }, [fetchReview]);
+
+  useEffect(() => {
+    fetchReview();
+  }, [infoNewReviewList, fetchReview]);
+
+  const handleDeleteReview = useCallback(
+    async (review_id: number) => {
+      try {
+        // console.log("리뷰 삭제 요청 시작:", review_id);
+        const token = getToken();
+        await axios.delete(INFO_REVIEW_DELETE(review_id), {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        // console.log("리뷰 삭제 요청 완료");
+        await fetchReview();
+        await fetchAverageRating();
+        alert("리뷰가 성공적으로 삭제되었습니다.");
+      } catch (error) {
+        console.error("리뷰 삭제 중 오류 발생:", error);
+        alert("리뷰 삭제 중 오류가 발생했습니다.");
+      }
+    },
+    [fetchReview, fetchAverageRating]
+  );
+
+  const handleEditSubmit = useCallback(
+    async (reviewId: number, content: string, rating: number) => {
+      try {
+        const token = getToken();
+        // console.log("리뷰 수정:" + reviewId, content, rating);
+        await axios.put(
+          INFO_REVEIW_UPDATE(reviewId), //오타
+          {
+            comment: content,
+            reviewRating: rating,
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        await fetchReview();
+        await fetchAverageRating();
+        alert("리뷰가 성공적으로 수정되었습니다.");
+      } catch (error) {
+        console.error("리뷰 수정 중 오류 발생:", error);
+        alert("리뷰 수정 중 오류가 발생했습니다.");
+      }
+    },
+    [fetchReview, fetchAverageRating]
+  );
+
+  return (
+    <div className="max-w-768 w-full h-px600 overflow-y-scroll my-0 mx-auto flex flex-col gap-px20">
+      {infoReviewList.map((data) => (
+        <div
+          key={data.reviewId}
+          className="relative flex justify-between w-full"
+        >
+          <InfoReviewComponent {...data} />
+          {currentUser !== null && currentUser.idx === data.memberIdx && (
+            <InfoReviewEditBox
+              reviewId={data.reviewId}
+              comment={data.comment}
+              rating={data.reviewRating}
+              onDelete={handleDeleteReview}
+              onEditSubmit={handleEditSubmit}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default InfoReviewList;
